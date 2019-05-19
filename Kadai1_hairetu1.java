3fe44a512be1b792500e641e7c944575fa70509c
KurodaIkki <ikkikuroda1124@gmail.com>
2019-05-19 14:13:55 +0900

    配列の作成の課題

---
+++ Kadai1_hairetu1.java
@@ -0,0 +1,27 @@
+package java6_hairetu;
+
+import java.util.ArrayList;
+
+public class Kadai1_hairetu1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		ArrayList<String> data = new ArrayList<String>();
+
+        data.add("10");
+        System.out.println(data.get(0));
+        data.add("100");
+        System.out.println(data.get(1));
+        data.add("soeda");
+        System.out.println(data.get(2));
+        data.add("hayashi");
+        System.out.println(data.get(3));
+        data.add("-20");
+        System.out.println(data.get(4));
+        data.add("118");
+        System.out.println(data.get(5));
+        data.add("END");
+        System.out.println(data.get(6));
+	}
+
+}
